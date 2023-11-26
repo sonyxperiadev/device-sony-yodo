b7a7159ae8de6296b09a07b19fa6506badb9596c
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2023-11-26 01:33:26 +0000

    Platform: Use ArmV9
    SM8550 Uses ArmV9 cores, using armv9-a CPU variant allows for armv9 specific optimization's to be used
    
    Signed-off-by: SpectreDev1 <Sayf.mohammad01@gmail.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT_RUNTIME := generic
 
