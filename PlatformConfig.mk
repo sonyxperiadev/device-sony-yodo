008dcfe206a8d2d8badc54f5724ea9e2329195ed
Pavel Dubrova <pashadubrova@gmail.com>
2026-03-20 04:23:43 +0200

    PlatformConfig: Move recovery.wipe to common
    recovery.wipe is common for all platforms and is
    placed in the common repository.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -30,7 +30,7 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 
 BOARD_BOOTCONFIG += androidboot.usbcontroller=a600000.dwc3
 
-TARGET_RECOVERY_WIPE := $(PLATFORM_COMMON_PATH)/rootdir/recovery.wipe
+# Recovery fstab
 TARGET_RECOVERY_FSTAB ?= $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/fstab.yodo
 
 # SELinux
