3e16e967b66e3f913280ba6be2ab04db35f50acd
Pavel Dubrova <pashadubrova@gmail.com>
2026-03-24 07:04:13 +0200

    platform Remove deprecated vndk_package
    VNDK is deprecated.
    
    https://source.android.com/docs/core/architecture/vndk#vndk-deprecation
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- platform.mk
+++ platform.mk
@@ -109,11 +109,6 @@ TARGET_USES_SDE := true
 # Platform has been launched on Android 13 (API level 33)
 PRODUCT_SHIPPING_API_LEVEL := 33
 
-# Treble
-# Include vndk/vndk-sp/ll-ndk modules
-PRODUCT_PACKAGES += \
-    vndk_package
-
 # Device Specific Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
